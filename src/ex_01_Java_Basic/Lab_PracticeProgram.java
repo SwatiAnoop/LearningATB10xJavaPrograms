4461864227fec390a0650ae439e08735b22b2616
Swati <swatianoop92@gmail.com>
2025-01-06 14:36:56 +0530

    6th Jan new

--- src/ex_01_Java_Basic/Lab_PracticeProgram.java
+++ src/ex_01_Java_Basic/Lab_PracticeProgram.java
@@ -1,4 +1,9 @@
 package ex_01_Java_Basic;
 
-public class Lab_PracticeProgram {
+public class Lab_PracticeProgram
+{
+    public static void main(String[] args) {
+        System.out.println("2+2");
+    }
+
 }
