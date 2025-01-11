a9e0661a14d431fbc7b3ab100cfc7c9202267e62
Swati <swatianoop92@gmail.com>
2025-01-11 15:18:47 +0530

    11 th jan

---
+++ src/ex_02_Java_Basics/Lab015_PrintfPractice.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics;
+
+public class Lab015_PrintfPractice {
+    public static void main(String[] args) {
+        String name = "swati";
+        int age = 30;
+        int grade = 'c';
+        float marks = 90.5f;
+        System.out.printf("Name: %s \n",name);
+        System.out.printf("Age: %s \n", age);
+        System.out.printf("Grade: %c \n", grade);
+        System.out.printf("Marks: %f \n", marks);
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab016_PrintfPractice.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab016_PrintfPractice {
+    public static void main(String[] args) {
+        int number = 10;
+        String name = "Swati";
+        char c = 'A';
+        float marks = 90.12f;
+        long Phno = 22992222222l;
+        System.out.printf("student name: %s \n", name);
+        System.out.printf("student garde: %c \n", c);
+        System.out.printf("Student marks: %f \n",marks);
+        System.out.printf("student phone number: %d \n", Phno);
+
+    }
+}
