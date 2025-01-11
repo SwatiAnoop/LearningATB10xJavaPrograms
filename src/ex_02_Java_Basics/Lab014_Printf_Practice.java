84ed601b21e180f546404860bcf2c53e44072a9d
Swati <swatianoop92@gmail.com>
2025-01-11 14:55:01 +0530

    table of 10

---
+++ src/ex_02_Java_Basics/Lab014_Printf_Practice.java
@@ -0,0 +1,21 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Printf_Practice {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.printf("10*1= %d \n", num*1);
+        System.out.printf("10*1= %d \n", num*2);
+        System.out.printf("10*1= %d \n", num*3);
+        System.out.printf("10*1= %d \n", num*4);
+        System.out.printf("10*1= %d \n", num*5);
+        System.out.printf("10*1= %d \n", num*6);
+        System.out.printf("10*1= %d \n", num*7);
+        System.out.printf("10*1= %d \n", num*8);
+        System.out.printf("10*1= %d \n", num*9);
+        System.out.printf("10*1= %d \n", num*10);
+
+
+    }
+
+
+}
